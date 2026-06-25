7771bf58e34a473042dcb762ded2a2c92ac5389c
Ubuntu <ubuntu@ip-172-31-47-238.ap-south-1.compute.internal>
2026-06-25 11:22:35 +0000

    configure Https and custom admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = (
+        "email",
+        "username",
+        "role",
+        "is_staff",
+        "is_active",
+    )
+
+    list_filter = (
+        "role",
+        "is_staff",
+        "is_active",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Platform Role",
+            {
+                "fields": (
+                    "role",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            "Platform Role",
+            {
+                "fields": (
+                    "role",
+                )
+            },
+        ),
+    )
